8cb3a7207370b61c2857a1a8682dcdf66858d3dc
AlbanyAvery <albanyavery@gmail.com>
2025-10-06 20:09:23 -0500

    Implementado CRUD completo de ventas

--- backend/routes/ventaRoutes.js
+++ backend/routes/ventaRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const ventaController = require("../controllers/ventaController");
 
+// Endpoints CRUD de ventas
 router.get("/", ventaController.obtenerVentas);
+router.post("/", ventaController.agregarVenta);
+router.put("/:id", ventaController.actualizarVenta);
+router.delete("/:id", ventaController.eliminarVenta);
 
 module.exports = router;
